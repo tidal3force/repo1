1478d20e0b1e01d8c922dfd7b07436a51b46262c
jjmm <josemcj@hotmail.com>
2024-11-27 23:55:06 +0100

    correccion lectura fichero

--- text.py
+++ text.py
@@ -1,4 +1,4 @@
-with open('G:\git\kraken\repo1\holamundo.py') as texto:
-    for lineas in texto:
-        print(lineas)
+with open('G:/git/kraken/repo1/holamundo.py') as texto:
+    for lines in texto:
+        print(lines)
 
